632ae9dd901b1bf80d3d25068edcdcfe7c2202a7
tanthaipotong@gmail.com <tanthaipotong@gmail.com>
2025-11-15 19:46:33 +0700

    update remote platern

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "picsum.photos",
       },
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+      },
     ],
   },
 };
